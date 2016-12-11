fba442a597441dfaf0613cd2f50ceb59067a8778
DuGuanhong <523678660@qq.com>
2016-12-11 21:25:29 +0800

    main page refreshed

--- src/WishAGoodDay/js/jquery-ui-1.9.2.custom.js
+++ src/WishAGoodDay/js/jquery-ui-1.9.2.custom.js
@@ -174,7 +174,26 @@ $.extend( $.expr[ ":" ], {
 		return ( isTabIndexNaN || tabIndex >= 0 ) && focusable( element, !isTabIndexNaN );
 	}
 });
-
+function focusable( element, isTabIndexNotNaN ) {
+	var map, mapName, img,
+		nodeName = element.nodeName.toLowerCase();
+	if ( "area" === nodeName ) {
+		map = element.parentNode;
+		mapName = map.name;
+		if ( !element.href || !mapName || map.nodeName.toLowerCase() !== "map" ) {
+			return false;
+		}
+		img = $( "img[usemap=#" + mapName + "]" )[0];
+		return !!img && visible( img );
+	}
+	return ( /input|select|textarea|button|object/.test( nodeName ) ?
+		!element.disabled :
+		"a" === nodeName ?
+			element.href || isTabIndexNotNaN :
+			isTabIndexNotNaN) &&
+		// the element and all of its ancestors must be visible
+		visible( element );
+}
 // support
 $(function() {
 	var body = document.body,
